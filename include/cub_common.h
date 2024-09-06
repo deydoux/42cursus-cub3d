62e4b1c516829e5375bc76fc4cd9e834c1205cdc
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-09-06 18:25:50 +0200

    Unify cub_defs and cub_types in cub_common

--- include/cub_common.h
+++ include/cub_common.h
@@ -6,18 +6,52 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/02 13:08:43 by deydoux           #+#    #+#             */
-/*   Updated: 2024/08/21 16:47:06 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/09/06 18:24:34 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef CUB_COMMON_H
 # define CUB_COMMON_H
-# include "cub_defs.h"
-# include "cub_types.h"
+# define _GNU_SOURCE
 # include "libft.h"
 # include "mlx.h"
 # include <math.h>
+# include <stdint.h>
 # include <stdio.h>
+
+# define CUB_ERROR			"\e[31mError\e[0m\n"
+# define CUB_KEY_UP			'w'
+# define CUB_KEY_LEFT		'a'
+# define CUB_KEY_DOWN		's'
+# define CUB_KEY_RIGHT		'd'
+# define CUB_PLACEHOLDER	"CUB PLACE HOLDER:"
+# define CUB_SIZE			128
+# define CUB_WINDOW_HEIGH	720
+# define CUB_WINDOW_WIDTH	1280
+# define DEG_RAD			(M_PI / 180)
+
+typedef struct s_img
+{
+	int			height;
+	int			width;
+	int			size_line;
+	uint32_t	*buffer;
+	void		*ptr;
+}	t_img;
+
+typedef struct s_cub
+{
+	char	**map;
+	double	dir;
+	double	x;
+	double	y;
+	size_t	map_height;
+	size_t	map_width;
+	t_img	frame;
+	t_img	minimap;
+	void	*mlx;
+	void	*win;
+}	t_cub;
 
 void	cub_destroy(t_cub cub);
 
