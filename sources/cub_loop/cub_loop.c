a4588d4f9c2ab24d10e3e36761be6695ab219acb
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-09-09 14:36:49 +0200

    Add collisions

--- sources/cub_loop/cub_loop.c
+++ sources/cub_loop/cub_loop.c
@@ -6,7 +6,7 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/08/21 14:39:53 by deydoux           #+#    #+#             */
-/*   Updated: 2024/09/09 14:15:11 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/09/09 14:34:55 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -40,19 +40,22 @@ static void	handle_key_press(t_cub *cub)
 	{
 		if (cub->keys[cub_key_down])
 			return ;
-		cub->x += cub->dx_move;
-		cub->y += cub->dy_move;
+		if (cub->map.buf[(int)cub->y][(int)(cub->x + cub->dx_move)] != '1')
+			cub->x += cub->dx_move;
+		if (cub->map.buf[(int)(cub->y + cub->dy_move)][(int)cub->x] != '1')
+			cub->y += cub->dy_move;
 	}
 	else if (cub->keys[cub_key_down])
 	{
-		cub->x -= cub->dx_move;
-		cub->y -= cub->dy_move;
+		if (cub->map.buf[(int)cub->y][(int)(cub->x - cub->dx_move)] != '1')
+			cub->x -= cub->dx_move;
+		if (cub->map.buf[(int)(cub->y - cub->dy_move)][(int)cub->x] != '1')
+			cub->y -= cub->dy_move;
 	}
 	else if (!angle_move)
 		return ;
-	// printf("a %f\nx %f\ny %f\n\n", cub->a, cub->x, cub->y);
 	mlx_put_image_to_window(cub->mlx, cub->win, cub->map.img.ptr, 0, 0);
-	mlx_pixel_put(cub->mlx, cub->win, round(cub->x * CUB_SIZE), round(cub->y * CUB_SIZE), 0xff0000);
+	mlx_pixel_put(cub->mlx, cub->win, (int)(cub->x * CUB_SIZE), (int)(cub->y * CUB_SIZE), 0xff0000);
 }
 
 int	cub_loop(t_cub *cub)
