5704a2b12f937bcb4eb9b18907019310d0b86266
Dorian Eydoux <dorian.eydoux@icloud.com>
2024-09-11 17:50:05 +0200

    Revert "Optimize cub_raycast"
    This reverts commit ddb05fb997c5f3c17446cad10875340871540d53.

--- sources/cub_loop/cub_raycast.c
+++ sources/cub_loop/cub_raycast.c
@@ -6,53 +6,28 @@
 /*   By: deydoux <deydoux@student.42lyon.fr>        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/09/11 16:04:59 by deydoux           #+#    #+#             */
-/*   Updated: 2024/09/11 17:39:33 by deydoux          ###   ########.fr       */
+/*   Updated: 2024/09/11 17:06:22 by deydoux          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "cub_loop.h"
 
-static double	calc_ray_dxy_neg(double xy, double dxy)
-{
-	return ((xy - (int)xy) / (dxy * -1));
-}
-
-static double	calc_ray_dxy_pos(double xy, double dxy)
-{
-	return (((int)xy - xy + 1) / dxy);
-}
-
-static double	calc_ray_dxy_nul(double xy, double dxy)
-{
-	(void)xy;
-	(void)dxy;
-	return (2);
-}
-
 void	cub_raycast(t_cub cub)
 {
-	t_calc_ray_dxy	calc_ray_dx;
-	t_calc_ray_dxy	calc_ray_dy;
-	t_cub_ray		ray;
+	t_cub_ray	ray;
 
-	if (cub.dx < 0)
-		calc_ray_dx = calc_ray_dxy_neg;
-	else if (cub.dx)
-		calc_ray_dx = calc_ray_dxy_pos;
-	else
-		calc_ray_dx = calc_ray_dxy_nul;
-	if (cub.dy < 0)
-		calc_ray_dy = calc_ray_dxy_neg;
-	else if (cub.dy)
-		calc_ray_dy = calc_ray_dxy_pos;
-	else
-		calc_ray_dy = calc_ray_dxy_nul;
 	ray.x = cub.x;
 	ray.y = cub.y;
 	while (cub.map.buf[(int)ray.y][(int)ray.x] != '1')
 	{
-		ray.dx = calc_ray_dx(ray.x, cub.dx);
-		ray.dy = calc_ray_dy(ray.y, cub.dy);
+		if (cub.dx < 0)
+			ray.dx = (ray.x - (int)ray.x) / (cub.dx * -1);
+		else
+			ray.dx = ((int)ray.x - ray.x + 1) / cub.dx;
+		if (cub.dy < 0)
+			ray.dy = (ray.y - (int)ray.y) / (cub.dy * -1);
+		else
+			ray.dy = ((int)ray.y - ray.y + 1) / cub.dy;
 		if (ray.dx < ray.dy)
 			ray.a = ray.dx;
 		else
